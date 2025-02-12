3740e4afcd11379a3ab02ff66937a4c5746a5a3f
KFrancis05 <jgf0803@gmail.com>
2025-02-12 18:01:30 -0500

    Added divison and multiplication as well as fixing a spelling error

--- Calculator.java
+++ Calculator.java
@@ -9,18 +9,24 @@ public class Calculator {
 
         r.nextLine();
 
-        System.out.println("Please enter an another int");
+        System.out.println("Please enter another int");
         int two = r.nextInt();
 
         r.nextLine();
 
-        System.out.println("Would you like to add or subtract the ints? A or S");
-        String answer = r.nextLine();
-        if(answer.equals('A') || answer.equals('a')){
+        System.out.println("What would you like to do? \n 1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide");
+        int answer = r.nextInt();
+        if(answer == 1){
             System.out.println("Sum of the two numbers: "+(one+two));
         }
-        else if(answer.equals('S') || answer.equals('s')){
+        else if(answer == 2){
             System.out.println("Difference of the two numbers: "+(one-two));
+        }
+        else if(answer == 3){
+            System.out.println("Product of the two numbers: "+(one*two));
+        }
+        else if(answer == 4){
+            System.out.println("Quotient of the two numbers: "+(one/two));
         }
         else{
             System.out.println("Invalid answer.");
