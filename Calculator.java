c0638bc1d96aac699a92e1642d8e9c5748258f08
KFrancis05 <jgf0803@gmail.com>
2025-02-12 17:45:58 -0500

    Option to add or subtract added

--- Calculator.java
+++ Calculator.java
@@ -12,6 +12,18 @@ public class Calculator {
         System.out.println("Please enter an another int");
         int two = r.nextInt();
 
-        System.out.println("Sum of the two numbers: "+(one+two));
+        r.nextLine();
+
+        System.out.println("Would you like to add or subtract the ints? A or S");
+        String answer = r.nextLine();
+        if(answer.equals('A') || answer.equals('a')){
+            System.out.println("Sum of the two numbers: "+(one+two));
+        }
+        else if(answer.equals('S') || answer.equals('s')){
+            System.out.println("Difference of the two numbers: "+(one-two));
+        }
+        else{
+            System.out.println("Invalid answer.");
+        }
     }
 }
